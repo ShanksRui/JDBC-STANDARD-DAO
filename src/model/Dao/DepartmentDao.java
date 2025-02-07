c71c8fce3a4931704c72cbfd90b8756d3bfae48c
Luiz <midoriya.7140@gmail.com>
2025-02-06 23:27:40 -0300

    add seller and Department DaoInterfaces

---
+++ src/model/Dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package model.Dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+}
