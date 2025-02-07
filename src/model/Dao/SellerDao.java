b75af77a64323bf17ee8210e7c07f48396863aef
Luiz Suo <147952455+ShanksRui@users.noreply.github.com>
2025-02-06 23:29:25 -0300

    Update SellerDao.java

--- src/model/Dao/SellerDao.java
+++ src/model/Dao/SellerDao.java
@@ -1,5 +1,15 @@
 package model.Dao;
+import java.util.List;
+
+import model.Entities.Department;
+import model.Entities.Seller;
 
 public interface SellerDao {
 
+  void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller Seller(Integer id);
+	List<Seller> findAll();
+  
 }
